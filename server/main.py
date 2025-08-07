b27817dcc73795831d43fe2ecc9800668f1e1408
Swara Saoji <swarasaoji1@gmail.com>
2025-08-07 15:00:31 +0530

    Enable CORS for deployed frontend

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://flashcard-app-frontend-gjf4.vercel.app/"],
+    allow_origins=["https://flashcard-app-frontend-gjf4.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
